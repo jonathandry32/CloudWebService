ca96129dbf674602fb5e9e03fe7dd51fd9c664d6
jonathandry32 <bayronkevin1@gmail.com>
2024-01-21 19:12:14 +0300

    validiter token 1 mois

--- src/main/java/com/vehicule/api/auth/JwtUtil.java
+++ src/main/java/com/vehicule/api/auth/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class JwtUtil {
 
     private final String secret_key = "deaonekineny";
-    private long accessTokenValidity = 60*60*1000;
+    private long accessTokenValidity = 30 * 24 * 60 * 60 * 1000;
 
     private final JwtParser jwtParser;
 
