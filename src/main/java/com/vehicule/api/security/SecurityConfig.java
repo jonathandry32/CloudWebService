d1d00a3d67ea386e9db4525182b240d820bf3a1a
jonathandry32 <bayronkevin1@gmail.com>
2024-01-26 14:32:04 +0100

    credential

--- src/main/java/com/vehicule/api/security/SecurityConfig.java
+++ src/main/java/com/vehicule/api/security/SecurityConfig.java
@@ -59,10 +59,12 @@ public class SecurityConfig {
     public class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type");
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("Authorization", "Content-Type")
+                    .allowCredentials(true)
+                    .maxAge(3600);
         }
     }
 
